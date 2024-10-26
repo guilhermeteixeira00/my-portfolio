d86c02a48ae3dc8ff1706e3da8a894194864901b
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-10-26 16:47:20 -0300

    update temp

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
 const username = document.getElementById("nome");
 const email = document.getElementById("email");
-const numero = document.getElementById("numero");
+/* const numero = document.getElementById("numero"); */
 const assunto = document.getElementById("assunto");
 const mensagem = document.getElementById("mensagem");
 
@@ -10,11 +10,15 @@ form.addEventListener("submit", (event) => {
 
     const usernameValue = username.value;
     const emailValue = email.value;
-    const numeroValue = numero.value;
+    /* const numeroValue = numero.value; */
     const assuntoValue = assunto.value;
     const mensagemValue = mensagem.value;
 
-    const url = ('https://wa.me/+5561999647021/?text= Nome: ' + usernameValue + ' Email: ' + emailValue + ' Numero: ' + numeroValue + ' Mensagem:' + mensagemValue);
+    const url = ('https://wa.me/+5561999647021/?text= Nome: ' + usernameValue + 
+        ' Email: ' + emailValue + 
+        ' Assunto: ' + assuntoValue +
+        /* ' Numero: ' + numeroValue +  */
+        ' Mensagem:' + mensagemValue);
 
     const win = window.open(url, '_blank')
     win.focus()
