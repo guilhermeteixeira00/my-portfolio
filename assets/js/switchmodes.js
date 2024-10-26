a87597c0d2e27dbb021f67afd4329c6be638d91c
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-10-26 13:26:48 -0300

    update fix color btn theme

--- assets/js/switchmodes.js
+++ assets/js/switchmodes.js
@@ -1,17 +1,20 @@
 let isDarkMode = true;
 
+
 function applyDarkMode() {
     const toggleDarkModeButton = document.getElementById("toggleDarkModeButton");
 
     if(isDarkMode)
         { /* parte dark mode */
         header.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+        slider.style.backgroundColor = "#4a4a4a";
         document.documentElement.style.setProperty('--bg-color', '#000000');
         document.documentElement.style.setProperty('--second-bg-color', '#161616');
         document.documentElement.style.setProperty('--text-color', 'snow');
     } else
     { /* parte white mode */
         header.style.backgroundColor = "rgba(79, 79, 79, 0.9)";
+        slider.style.backgroundColor = "#000000";
         document.documentElement.style.setProperty('--bg-color', '#cacaca');
         document.documentElement.style.setProperty('--second-bg-color', '#797979');
         document.documentElement.style.setProperty('--text-color', '#000000');
