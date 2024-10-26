6229b835deb48cd564a70ba894c7f3fb3a744fe9
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-10-26 13:18:07 -0300

    new update fix themes js

--- assets/js/switchmodes.js
+++ assets/js/switchmodes.js
@@ -3,22 +3,18 @@ let isDarkMode = true;
 function applyDarkMode() {
     const toggleDarkModeButton = document.getElementById("toggleDarkModeButton");
 
-    if(isDarkMode){
-        document.body.style.backgroundColor = "var(--bg-color)";
-        document.body.style.color = "snow";  
+    if(isDarkMode)
+        { /* parte dark mode */
         header.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
-        servicos.style.backgroundColor = "var(--second-bg-color)";
-        projetos.style.backgroundColor = "var(--bg-color)";
-        skills.style.backgroundColor = "var(--second-bg-color)";
-        aprendizado.style.backgroundColor = "var(--bg-color)";
-    } else{
-        document.body.style.backgroundColor = "rgb(225, 225, 225)";
-        document.body.style.color = "#000000";
+        document.documentElement.style.setProperty('--bg-color', '#000000');
+        document.documentElement.style.setProperty('--second-bg-color', '#161616');
+        document.documentElement.style.setProperty('--text-color', 'snow');
+    } else
+    { /* parte white mode */
         header.style.backgroundColor = "rgba(79, 79, 79, 0.9)";
-        servicos.style.backgroundColor = "rgb(79, 79, 79)";
-        projetos.style.backgroundColor = "rgb(225, 225, 225)";
-        skills.style.backgroundColor = "rgb(79, 79, 79)";
-        aprendizado.style.backgroundColor = "rgb(225, 225, 225)";
+        document.documentElement.style.setProperty('--bg-color', '#cacaca');
+        document.documentElement.style.setProperty('--second-bg-color', '#797979');
+        document.documentElement.style.setProperty('--text-color', '#000000');
     }
 }
 
