9b3ef4c55c46c95474ea2d6b03567c690ffb3fd2
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-10-28 22:20:39 -0300

    update test rain

--- assets/js/rain.js
+++ assets/js/rain.js
@@ -9,13 +9,12 @@ function rain(){
     let e = document.createElement('div');
     e.classList.add('drop');
     cloud.appendChild(e);
-
-    let left = Math.floor(Math.random() * 2000)
+    let left = Math.floor(Math.random() * 100)
     let size = Math.random() * 1.5;
     let duration = Math.random() *1;
 
     e.innerText = randomText();
-    e.style.left = left + 'px';
+    e.style.left = left + 'vw';
     e.style.fontSize = 0.5 + size + 'em';
     e.style.animationDuration = 1 + duration + 's';
 
