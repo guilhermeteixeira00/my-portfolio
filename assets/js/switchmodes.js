04644724d4efcd8036b17e3c4517367df46bb3fd
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-11-08 19:53:04 -0300

    script block devtools

--- assets/js/switchmodes.js
+++ assets/js/switchmodes.js
@@ -36,3 +36,14 @@ function toggleDarkMode(){
     isDarkMode = !isDarkMode;
     applyDarkMode();
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
+        event.preventDefault(); // Impede o menu de contexto de aparecer
+    }
+});
+  
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault(); // Impede o menu de contexto de aparecer
+});
+  
