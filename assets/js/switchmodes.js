e2e8e4da6d5609a748d10b979e0ec4b4b51afca1
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-11-08 20:04:08 -0300

    update fix

--- assets/js/switchmodes.js
+++ assets/js/switchmodes.js
@@ -36,14 +36,4 @@ function toggleDarkMode(){
     isDarkMode = !isDarkMode;
     applyDarkMode();
 }
-
-document.addEventListener('keydown', function(event) {
-    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
-        event.preventDefault(); // Impede o menu de contexto de aparecer
-    }
-});
-  
-document.addEventListener('contextmenu', function(event) {
-    event.preventDefault(); // Impede o menu de contexto de aparecer
-});
   
