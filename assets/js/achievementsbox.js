193d73a8cf2ca6a980d44f5acbac8551e47a5a62
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-11-20 01:45:53 -0300

    update first box achievements

---
+++ assets/js/achievementsbox.js
@@ -0,0 +1,9 @@
+const exitbox = document.querySelector(".boxsair");
+
+galaxybrain.addEventListener('click', () => {
+    card1.style.display = 'flex';
+})
+
+exitbox.addEventListener('click', () => {
+    card1.style.display = 'none';
+})
