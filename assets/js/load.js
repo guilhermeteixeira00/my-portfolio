faa71c85e55755232b001694ea52752b41c188b8
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2025-05-01 18:59:50 -0300

    Remover comentários do código de carregamento e ajustar a exibição do loader no HTML

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,6 +1,6 @@
-/* window.addEventListener('load', function () {
+window.addEventListener('load', function () {
   setTimeout(function () {
     document.getElementById('loader').style.display = 'none';
     document.getElementById('conteudo').style.display = 'block';
   }, 5000);
-}); */
+});
